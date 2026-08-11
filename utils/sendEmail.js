7b3134733bfdb2182f521f256e6cc9172eedab1d
Daniel- new <bassygit@gmail.com>
2026-08-11 17:58:28 +0100

    fixing sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,23 +1,15 @@
 import nodemailer from 'nodemailer';
-import dns from 'node:dns';
 
 const sendEmail = async ({ to, subject, html }) => {
             try {
                         // create transporter
                         const transporter = nodemailer.createTransport({
                                     host: process.env.EMAIL_HOST,
-                                    port: parseInt(process.env.EMAIL_PORT) || 587,
-                                    secure: false, // false for port 587
+                                    port: process.env.EMAIL_PORT,
+                                    secure: false,
                                     auth: {
                                                 user: process.env.EMAIL_USER,
                                                 pass: process.env.EMAIL_PASS
-                                    },
-                                    // THIS FORCES DNS TO RESOLVE ONLY IPV4 ADDRESSES
-                                    lookup: (hostname, options, callback) => {
-                                                return dns.lookup(hostname, { family: 4 }, callback);
-                                    },
-                                    tls: {
-                                                rejectUnauthorized: false
                                     }
                         });
 
@@ -39,39 +31,3 @@ const sendEmail = async ({ to, subject, html }) => {
 };
 
 export default sendEmail;
-
-
-
-// import nodemailer from 'nodemailer';
-
-// const sendEmail = async ({ to, subject, html }) => {
-//             try {
-//                         // create transporter
-//                         const transporter = nodemailer.createTransport({
-//                                     host: process.env.EMAIL_HOST,
-//                                     port: process.env.EMAIL_PORT,
-//                                     secure: false,
-//                                     auth: {
-//                                                 user: process.env.EMAIL_USER,
-//                                                 pass: process.env.EMAIL_PASS
-//                                     }
-//                         });
-
-//                         // send email
-//                         const info = await transporter.sendMail({
-//                                     from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_USER}>`,
-//                                     to,
-//                                     subject,
-//                                     html
-//                         });
-
-//                         console.log('Email sent:', info.messageId);
-//                         return info;
-
-//             } catch (error) {
-//                         console.error('Email error:', error);
-//                         throw new Error('Email could not be sent');
-//             }
-// };
-
-// export default sendEmail;
