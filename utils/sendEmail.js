c5e7380281b93bdcb5df5af648d4bc3c464b4d39
Daniel- new <bassygit@gmail.com>
2026-08-11 12:40:36 +0100

    fix on email service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,4 +1,8 @@
 import nodemailer from 'nodemailer';
+import dns from 'node:dns';
+
+// Force Node.js to prefer IPv4 when looking up the email host address
+dns.setDefaultResultOrder('ipv4first');
 
 const sendEmail = async ({ to, subject, html }) => {
             try {
@@ -31,3 +35,42 @@ const sendEmail = async ({ to, subject, html }) => {
 };
 
 export default sendEmail;
+
+
+
+
+
+
+// import nodemailer from 'nodemailer';
+
+// const sendEmail = async ({ to, subject, html }) => {
+//             try {
+//                         // create transporter
+//                         const transporter = nodemailer.createTransport({
+//                                     host: process.env.EMAIL_HOST,
+//                                     port: process.env.EMAIL_PORT,
+//                                     secure: false,
+//                                     auth: {
+//                                                 user: process.env.EMAIL_USER,
+//                                                 pass: process.env.EMAIL_PASS
+//                                     }
+//                         });
+
+//                         // send email
+//                         const info = await transporter.sendMail({
+//                                     from: `"${process.env.EMAIL_FROM_NAME}" <${process.env.EMAIL_USER}>`,
+//                                     to,
+//                                     subject,
+//                                     html
+//                         });
+
+//                         console.log('Email sent:', info.messageId);
+//                         return info;
+
+//             } catch (error) {
+//                         console.error('Email error:', error);
+//                         throw new Error('Email could not be sent');
+//             }
+// };
+
+// export default sendEmail;
